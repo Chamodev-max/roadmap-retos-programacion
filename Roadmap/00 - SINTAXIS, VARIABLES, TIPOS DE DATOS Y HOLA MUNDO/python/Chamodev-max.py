88712efa8c4c0eb6ff486bc2719a830db384643a
Chamodev-max <jose.devfit@gmail.com>
2025-07-03 23:19:23 +0000

    00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Chamodev-max.py
@@ -0,0 +1,57 @@
+""" /*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+"""
+
+# Comentario: https://www.python.org/
+"""
+Comentarios
+varias
+lineas
+"""
+'''
+Comentarios 
+varias 
+lineas
+'''
+mi_variable_texto = "mi variable"; #string
+print (mi_variable_texto);
+print (type (mi_variable_texto));
+
+mi_variable_entero = 5; #Entero
+print (mi_variable_entero);
+print (type(mi_variable_entero));
+
+mi_variable_comlejo = 5j; #complejo
+print (mi_variable_comlejo);
+print (type(mi_variable_comlejo));
+
+mi_variable_flotante = 5.8; #Float
+print (mi_variable_flotante);
+print (type(mi_variable_flotante));
+
+mi_variable_boleano = False; #Bolean
+print (mi_variable_boleano);
+print (type(mi_variable_boleano));
+
+print ("Hola Python");
